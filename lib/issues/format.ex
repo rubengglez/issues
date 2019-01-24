81f8b902a11d115a70a3cdae8e76b54099ba219b
Rubén <ruben.garcia@quobis.com>
2019-01-24 17:10:49 +0100

    Add doc example in function

--- lib/issues/format.ex
+++ lib/issues/format.ex
@@ -1,4 +1,11 @@
 defmodule Issues.Format do
+  @doc """
+    Sort a list of maps using the created_at field contained in each map in ascending way
+
+    ## Examples
+      iex(1)> Issues.Format.sort([%{"id" => 2, "created_at" => 3}, %{"id" => 4, "created_at" => 2}])
+      [%{"created_at" => 2, "id" => 2}, %{"created_at" => 3, "id" => 2}]
+  """
   def sort(list_of_maps) do
     Enum.sort(list_of_maps, fn a, b ->
       a["created_at"] <= b["created_at"]
